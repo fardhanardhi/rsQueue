c8c61e0d1f3f858f8eb34cbbb933ede321be2d4a
Fardhan Ardhi <fa.dsg.id@gmail.com>
2018-06-22 18:43:16 +0700

    Setup node class

--- NodePasien.java
+++ NodePasien.java
@@ -1,5 +1,14 @@
 package rsQueue;
 
 public class NodePasien {
-    
+    String nama;
+    int noAntrian;
+    NodePasien next, prev;
+
+    NodePasien(NodePasien prev, String nama, int noAntrian, NodePasien next) {
+        this.prev = prev;
+        this.nama = nama;
+        this.noAntrian = noAntrian;
+        this.next = next;
+    }
 }
--- NodeTransaksi.java
+++ NodeTransaksi.java
@@ -1,5 +1,16 @@
 package rsQueue;
 
 public class NodeTransaksi {
+    NodePasien pasien;
+    String keluhan;
+    int jumlahBayar;
+    NodeTransaksi next,prev;
     
+    NodeTransaksi(NodeTransaksi prev, NodePasien pasien, String keluhan, int jumlahBayar, NodeTransaksi next) {
+        this.prev = prev;
+        this.pasien = pasien;
+        this.keluhan = keluhan;
+        this.jumlahBayar = jumlahBayar;
+        this.next = next;
+    }
 }
